5ce30f712c78c1937297cb31529a17473fa03363
allison <allisonchen1130@gmail.com>
2020-04-03 22:13:08 -0400

    Input field and submit button

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 
+import UserCreate from "../components/UserCreate";
+
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          <i
+            className="flag ca"
+            onClick={() => this.onLanguageChange("English/French")}
+          />
+          <i
+            className="flag cn"
+            onClick={() => this.onLanguageChange("Mandarin")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 
