5760c12e3ca992d3c3c4fac39e31c88ec267103c
allison <allisonchen1130@gmail.com>
2020-04-04 22:49:29 -0400

    context value example

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import LangugageContext from "../contexts/LanguageContext";
+
 class Field extends React.Component {
+  static contextType = LangugageContext;
+
   render() {
+    const text = this.context === "english" ? "Name" : "姓名";
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
