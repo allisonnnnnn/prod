6de958216d58402be4fdbfb87eb0481f0fc9494a
allison <allisonchen1130@gmail.com>
2020-04-05 23:24:50 -0400

    context.provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import UserCreate from "../components/UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -22,7 +23,9 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("Mandarin")}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
